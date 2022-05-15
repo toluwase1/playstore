45e232386bc4154baa079abc05352fbf0d6584bd
toluwase <toluwasethomas1@gmail.com>
2022-05-15 16:43:51 +0100

    grouping and reconstructing handlers

--- api/rest/rest.go
+++ api/rest/rest.go
@@ -4,25 +4,41 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func RunAPI(address string) error {
+func RunAPIWithHandler(address string, h HandlerInterface) error {
 	//Get gin's default engine
 	r := gin.Default()
-	//Define a handler
-	h, _ := NewHandler()
 	//get products
 	r.GET("/products", h.GetProducts)
 	//get promos
 	r.GET("/promos", h.GetPromos)
+
 	//post user sign in
-	r.POST("/users/signin", h.SignIn)
-	//add a user
-	r.POST("/users", h.AddUser)
+	r.POST("/user/signin", h.SignIn)
 	//post user sign out
+
 	r.POST("/user/:id/signout", h.SignOut)
 	//get user orders
 	r.GET("/user/:id/orders", h.GetOrders)
 	//post purchase charge
-	r.POST("/users/charge", h.Charge)
-	//run the server
+	r.POST("/user/charge", h.Charge)
+	userGroup := r.Group("/user")
+	{
+		userGroup.POST("/:id/signout", h.SignOut)
+		userGroup.GET("/:id/orders", h.GetOrders)
+	}
+	usersGroup := r.Group("/users")
+	{
+		usersGroup.POST("/charge", h.Charge)
+		usersGroup.POST("/signin", h.SignIn)
+		usersGroup.POST("", h.AddUser)
+	}
 	return r.Run(address)
+}
+
+func RunAPI(address string) error {
+	h, err := NewHandler()
+	if err != nil {
+		return err
+	}
+	return RunAPIWithHandler(address, h)
 }
