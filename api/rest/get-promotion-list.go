46a323cfa7348ce56c34f65dcdb14738abd819ca
toluwase <toluwasethomas1@gmail.com>
2022-05-15 16:21:13 +0100

    get promotion list handler done

---
+++ api/rest/get-promotion-list.go
@@ -0,0 +1,18 @@
+package rest
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func (h *Handler) GetPromos(c *gin.Context) {
+	if h.db == nil {
+		return
+	}
+	promos, err := h.db.GetPromos()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, promos)
+}
