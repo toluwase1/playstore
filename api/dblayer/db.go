483bdc976a24bddc35d9045e1c552f19ab59059a
toluwase <toluwasethomas1@gmail.com>
2022-05-15 15:55:16 +0100

    db layer and rest package

---
+++ api/dblayer/db.go
@@ -0,0 +1,15 @@
+package dblayer
+
+import "github.com/toluwase1/playstore/models"
+
+type DBLayer interface {
+	GetAllProducts() ([]models.Product, error)
+	GetPromos() ([]models.Product, error)
+	GetCustomerByName(string, string) (models.Customer, error)
+	GetCustomerByID(int) (models.Customer, error)
+	GetProduct(uint) (models.Product, error)
+	AddUser(models.Customer) (models.Customer, error)
+	SignInUser(username, password string) (models.Customer, error)
+	SignOutUserById(int) error
+	GetCustomerOrdersByID(int) ([]models.Order, error)
+}
---
+++ api/rest/rest.go
@@ -0,0 +1,4 @@
+package rest
+
+func RunAPI(address string) error {
+}
