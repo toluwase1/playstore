69e34f4a042817280dde2772b80a36d096e73451
toluwase <toluwasethomas1@gmail.com>
2022-05-15 16:37:13 +0100

    reworking routes and charge credit card in progress

--- api/rest/rest.go
+++ api/rest/rest.go
@@ -1,44 +1,28 @@
 package rest
 
 import (
-	"errors"
 	"github.com/gin-gonic/gin"
 )
 
 func RunAPI(address string) error {
-	r := gin.Default() // the gin engine
-	r.GET("/relativepath/to/url", func(c *gin.Context) {
-		//take action
-	})
+	//Get gin's default engine
+	r := gin.Default()
+	//Define a handler
+	h, _ := NewHandler()
 	//get products
-	r.GET("/products", func(c *gin.Context) {
-		//return a list of all products to the client
-	})
+	r.GET("/products", h.GetProducts)
 	//get promos
-	r.GET("/promos", func(c *gin.Context) {
-		//return a list of all promotions to the client
-	})
+	r.GET("/promos", h.GetPromos)
 	//post user sign in
-	r.POST("/users/signin", func(c *gin.Context) {
-		//sign in a user
-	})
-	//add user
-	r.POST("/users", func(c *gin.Context) {
-		//add a user
-	})
-	r.POST("/user/:id/signout", func(c *gin.Context) {
-		//sign out a user with the provided id
-	})
-
+	r.POST("/users/signin", h.SignIn)
+	//add a user
+	r.POST("/users", h.AddUser)
+	//post user sign out
+	r.POST("/user/:id/signout", h.SignOut)
 	//get user orders
-	r.GET("/user/:id/orders", func(c *gin.Context) {
-		//get all orders belonging to the provided user id
-	})
-
+	r.GET("/user/:id/orders", h.GetOrders)
 	//post purchase charge
-	r.POST("/users/charge", func(c *gin.Context) {
-		//charge credit card for user
-	})
-
-	return errors.New("")
+	r.POST("/users/charge", h.Charge)
+	//run the server
+	return r.Run(address)
 }
