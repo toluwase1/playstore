7d4cd2aa127b60cee0901e67525ca46602821968
toluwase <toluwasethomas1@gmail.com>
2022-05-15 14:26:05 +0100

    customer and order models

---
+++ api/models/customer.go
@@ -0,0 +1,8 @@
+package models
+
+type Customer struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Email     string `json:"email"`
+	LoggedIn  bool   `json:"loggedin"`
+}
---
+++ api/models/order.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type Order struct {
+	Product
+	Customer
+	CustomerID   int       `json:"customer_id"`
+	ProductID    int       `json:"product_id"`
+	Price        float64   `json:"sell_price"`
+	PurchaseDate time.Time `json:"purchase_date"`
+}
